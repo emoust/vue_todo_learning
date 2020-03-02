769c696f8aa744761575becf947ad398f1526ab2
lincy <owo@emoust.com>
2020-03-02 23:50:00 +0800

    Write a basicially todo manager which not able to use.....

---
+++ js/main.js
@@ -0,0 +1,81 @@
+var todo_list = [];
+//todos = {{id:num.,text:"text",is_finished:true/false}}
+
+function is_empty(obj) {
+	for (var key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+var h1 = new Vue({
+	el: '#h1',
+	data: {
+
+	}
+});
+
+var clear_finished = new Vue({
+	el: '#clear_finished',
+	methods: {
+		clear: function() {
+			console.log("Begin to clear:");
+			var todo_list_new = [];
+			var id = 0;
+			for (var todo in todo_list) {
+				
+				if (todo.is_finished === false) {
+					todo_list_new.push({
+						id: id,
+						text: todo.text,
+						is_finished : false
+					});
+					id++;
+				}
+				todo_list = todo_list_new;
+			}
+		}
+	}
+});
+
+//These are for we use templates instead of directly usage.
+
+// Vue.component('todo-item', {
+// 	props: ['todo'],
+// 	template: '<li>{{todo.text}}</li>'
+// })
+
+var todos_display = new Vue({
+	el: '#todos_display',
+	data: {
+		todo_list: todo_list,
+		this_id: 0,
+		if_already_have: is_empty(todo_list)		
+	}
+	// methods: {
+	// 	change_state: function() {
+	// 		var id = this.this_id;
+	// 		console.log(id);
+	// 		todo_list[id].is_finished = true;
+	// 	}
+	//}
+});
+
+var add_todo = new Vue({
+	el: '#add_todo',
+	data: {
+		text: ''
+	},
+	methods: {
+		push_todo: function() {
+			todo_list.push({
+				id: todo_list.length,
+				text: this.text,
+				is_finished: false
+			});
+			this.text = '';
+		}
+	}
+});
---
+++ todo.md
@@ -0,0 +1,6 @@
+# What to do of this file
+- Write an todo.
+- Modified the todo message.
+- Delete the todos.
+- Set an todo to finishing condition.
+- Fast-remove the todos already finished.
